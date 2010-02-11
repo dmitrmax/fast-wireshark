383d58878c55b826448b9158d3eff1d1965c2ffe
grencez <grencez@8d1846ee-05f3-11df-adb7-1be7f7e3f636>
2010-02-11 05:29:27 +0000

    Test script is saner than ever before. Proccess spawning has been modularized. There is no good way to play with TShark other than waiting a bit for it to initialize, and then waiting a bit for it to capture packets beforke killing it.

--- test/simple.sh
+++ test/simple.sh
@@ -12,67 +12,97 @@ fi
 PORT="$1"
 TSHARK="$2"
 
-dir=`mktemp -d` || exit 1
+dir=$(mktemp -d --tmpdir "fast-wireshark.XXXXXX") || exit 1
 textf="$dir/text"
 
-client_text="message from client"
+# Will turn to 1 if a test fails
+# This is our exit status
+STATUSP=0
 
-cleanup_script ()
+start_tshark ()
 {
-    local pids
-    pids=$(cut -d ' ' -f '2' "$dir/pids")
+    $TSHARK -i lo -T fields -e fast.text -l \
+     2>> "$dir/stderrTshark" \
+     &
 
-    echo "killing: $pids"
-    kill $pids
+    # Grab TShark's pid
+    printf 'tshark\t%d\n' $! >> "$dir/pids"
+
+    # Wait for TShark to be ready
+    sleep 1
+}
+
+start_server ()
+{
+    {
+        ./server $PORT localhost \
+         2>> "$dir/stderrServ" \
+         &
+
+        # Grab server's pid
+        printf 'server\t%d\n' $! >> "$dir/pids"
+    } \
+    | \
+    {
+        # Wait for server to be ready
+        read -r listenerc anon \
+        || \
+        {
+            echo "Problem starting server, see error log in $dir" >&2
+            return 1
+        }
+
+        echo "Server started, we have $listenerc listeners"
+        return 0
+    }
+} >> "$dir/logScript"
+
+stop_process ()
+{
+    local name
+    name=$1
+    echo "killing $name"
+
+    # Kill the specified process
+    kill $(grep -e "$name" < "$dir/pids" | cut -f 2)
+
+    # Remove its corresponding record
+    sed -i -e "/$name/d" "$dir/pids"
 
 } 2>&1 >> "$dir/logScript"
 
-#timeout 3 
-{
-    $TSHARK -i lo -T fields -e fast.text -l \
-     > "$dir/stdoutTshark" \
-     2>&3 \
-     &
-    # Grap TShark's pid
-    echo "tshark $!" >> "$dir/pids"
-} 3>&1 \
-| \
-{ # IN: TShark's stderr 
+
+client_text="message from client"
+
+start_server || exit 1
+start_tshark >> "$dir/stdoutTshark"
     
-    # Wait for TShark to be ready
-    read -r useless || exit 1
+# So run client
+./client "$client_text" $PORT localhost 2>> "$dir/stderrClient"
 
-    ./server $PORT localhost 2> "$dir/stderrServ" &
-    echo "server $!" >> "$dir/pids"
-} \
-| \
-{ # IN: Server's stdout
-    read -r listenerc anon || exit 1
-    echo "we have $listenerc listeners" >> "$dir/logScript"
-    # Server is now listening
-    # So run client
-    ./client "$client_text" $PORT localhost 2> "$dir/stderrClient"
-}
-
+# Give TShark some time to collect the traffic
 sleep 2
-statusp=0
 
 # See if TShark got client_text
-grep -e "$client_text" "$dir/stdoutTshark" \
+grep -e '^'"${client_text}"'$' "$dir/stdoutTshark" \
  > /dev/null \
 &&   echo "Simple exchange PASSED" >&2 \
-|| { echo "Simple exchange FAILED" >&2 ; statusp=1 ; }
+|| { echo "Simple exchange FAILED" >&2 ; STATUSP=1 ; }
+
+# Stop spawned processes
+stop_process tshark
+stop_process server
 
 # Display test result
-if [ $statusp -eq 0 ]
+if [ $STATUSP -eq 0 ]
 then
     echo "ALL TESTS PASSED"
+    rm -r "$dir"
     # TODO: Do we care about cleaning up the temp file wireshark makes?
 else
     echo "SOME TESTS FAILED"
     echo "check $dir for relevant files"
 fi
 
-cleanup_script
-exit $statusp
+exit $STATUSP
 
