6740eed337b66091bd2735cbdbc1ad3b0f2b25dd
grencez <grencez@8d1846ee-05f3-11df-adb7-1be7f7e3f636>
2010-02-11 18:47:15 +0000

    Worked around some timing issues with tshark. Add more tests and intelligent error reporting.

--- test/simple.sh
+++ test/simple.sh
@@ -5,23 +5,35 @@
 
 if [ $# -ne 2 ]
 then
-    echo "Usage: $0 PORT TSHARK"
+    echo "Usage: $0 port [tshark]" >&2
     exit 1
 fi
 
+###### BEGIN GLOBAL VARIABLES ######
+
 PORT="$1"
-TSHARK="$2"
+
+TSHARK=tshark
+
+if [ -n "$2" ]
+then
+    TSHARK="$2"
+fi
 
 dir=$(mktemp -d --tmpdir "fast-wireshark.XXXXXX") || exit 1
-textf="$dir/text"
+
+outDTshark="$dir/tshark_output"
+mkdir "$outDTshark" || exit 1
 
 # Will turn to 1 if a test fails
 # This is our exit status
 STATUSP=0
 
+###### END GLOBAL VARIABLES ######
+
 start_tshark ()
 {
-    $TSHARK -i lo -T fields -e fast.text -l \
+    $TSHARK -i lo -l $@ \
      2>> "$dir/stderrTshark" \
      &
 
@@ -30,6 +42,16 @@ start_tshark ()
 
     # Wait for TShark to be ready
     sleep 1
+}
+
+stop_tshark ()
+{
+    # Wait for TShark to be ready
+    sleep 1
+    stop_process tshark
+
+    # TShark needs time to flush buffers
+    sleep .2
 }
 
 start_server ()
@@ -68,41 +90,126 @@ stop_process ()
 
     # Remove its corresponding record
     sed -i -e "/$name/d" "$dir/pids"
-
 } 2>&1 >> "$dir/logScript"
 
+run_client ()
+{
+    ./client "$@" $PORT localhost
+} 2>> "$dir/stderrClient"
 
-client_text="message from client"
+process_test_result ()
+{
+    local nameTest status
+    nameTest=$1
+    status=$2
+
+    if [ "$status" -eq 0 ]
+    then
+        printf 'TEST: %s PASS\n' "$nameTest"
+    else
+        printf 'TEST: %s FAIL\n' "$nameTest"
+        STATUSP=1
+    fi
+    return $status
+} >&2
+
+script_is_done ()
+{
+    # Stop spawned server
+    stop_process server
+
+    # Display test result
+    if [ $STATUSP -eq 0 ]
+    then
+        echo 'ALL TESTS PASSED'
+        rm -r "$dir"
+        # TODO: Do we care about cleaning up the temp file wireshark makes?
+    else
+        echo 'SOME TESTS FAILED'
+        echo '\-- Hint: Check the first failure'
+        echo "\-- See $dir for relevant files"
+    fi
+
+    return $STATUSP
+} >&2
 
 start_server || exit 1
-start_tshark >> "$dir/stdoutTshark"
-    
-# So run client
-./client "$client_text" $PORT localhost 2>> "$dir/stderrClient"
 
-# Give TShark some time to collect the traffic
-sleep 2
+##### TEST: TShark Installed
+# See if we can even execute TShark
+command -v "$TSHARK" > /dev/null
+process_test_result 'TShark Installed' $? \
+|| \
+{
+    STATUSP=1
+    echo '\-- You do not even have tshark installed >:('
+    if command -v wireshark > /dev/null
+    then
+        echo '\-- But the strange part is, you do have wireshark!'
+    fi
+    echo '\-- I will just exit now...'
+    script_is_done
+    exit $?
+}
 
-# See if TShark got client_text
-grep -e '^'"${client_text}"'$' "$dir/stdoutTshark" \
- > /dev/null \
-&&   echo "Simple exchange PASSED" >&2 \
-|| { echo "Simple exchange FAILED" >&2 ; STATUSP=1 ; }
 
-# Stop spawned processes
-stop_process tshark
-stop_process server
+###### TEST: Plugin Recognition
+# See if the plugin is even recognized
+{
+    stdoutTshark="$outDTshark/plugin_recognition"
 
-# Display test result
-if [ $STATUSP -eq 0 ]
-then
-    echo "ALL TESTS PASSED"
-    rm -r "$dir"
-    # TODO: Do we care about cleaning up the temp file wireshark makes?
-else
-    echo "SOME TESTS FAILED"
-    echo "check $dir for relevant files"
-fi
+    start_tshark > "$stdoutTshark"
+    run_client 'hello there server!'
+    stop_tshark
 
-exit $STATUSP
+    # TODO: this combo is ideal but doesn't work! why?
+    # start_tshark -T fields -e udp > "$stdoutTshark"
+    # sed -i -e 's/^.*Dst Port:\([^(]*\)(\([^)]*\)).*$/\1 \2/' "$stdoutTshark"
+    # grep -e "$PORT" "$stdoutTshark"
+
+    grep -e 'FAST' "$stdoutTshark" > /dev/null
+
+    process_test_result 'Plugin Recognition' $? \
+    || \
+    {
+        if [ -x "$HOME/.wireshark/plugins/packet-fast.so" ]
+        then
+            echo '\-- But it looks like the plugin is installed'
+
+            grep -e '.' "$stdoutTshark" > /dev/null \
+            && echo '\-- ... but tshark did pick something else up in its'  \
+                    'place so there is definitely a problem with the plugin.' \
+            || echo '\-- ... but tshark did not pick up anything' \
+                    'so perhaps we killed it prematurely.'
+        else
+            echo '\-- Oh, the plugin is not installed, it should' \
+                 "be located at $HOME/.wireshark/plugins/packet-fast.so"
+        fi
+    } >&2
+}
+
+
+###### TEST: Plain Field Recognition
+# See if the plugin can echo a text message
+{
+    stdoutTshark="$outDTshark/plain_field_recognition"
+    client_text="message from client"
+
+    start_tshark -T fields -e udp.dstport -e fast.text > "$stdoutTshark"
+    run_client "$client_text"
+    stop_tshark
+
+    # See if TShark got client_text
+    grep -e '^'"${PORT}.*${client_text}"'$' "$stdoutTshark" > /dev/null
+
+    process_test_result 'Plain Field Recognition' $? \
+    || \
+    {
+        grep -e "$PORT" "$stdoutTshark" > /dev/null \
+        || echo '\-- ... but it looks like we killed tshark prematurely'
+    } >&2
+}
+
+script_is_done
+exit $?
 
