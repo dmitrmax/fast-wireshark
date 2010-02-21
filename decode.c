9ec93508b59e8b6dfd01a052bf9fa11073eb7a40
jwf3188 <jwf3188@8d1846ee-05f3-11df-adb7-1be7f7e3f636>
2010-02-21 22:31:10 +0000

    added low-level decoding routines

---
+++ decode.c
@@ -0,0 +1,260 @@
+/**
+	@file	decode.c
+	@brief	low-level bit decoding routines
+	@author	Wes Fournier
+
+	|
+*/
+
+#include "decode.h"
+
+#define STOP_BIT 0x80
+#define SIGN_BIT 0x40
+
+gint count_encoded_bytes(const guint8* in, guint max_in_bytes)
+{
+	int i;
+	for(i=0;i<max_in_bytes;i++)
+	{
+		if(in[i]&STOP_BIT)
+		{
+			return i+1;
+		}
+	}
+	return -1;
+}
+
+gint decode_int16(
+	const guint8* in,
+	guint max_in_bytes,
+	gint16* out)
+{
+	if(count_encoded_bytes(in,max_in_bytes)!=sizeof(gint16))
+	{
+		return -1;
+	}
+
+	gint16 ret;
+
+	ret= in[0] & ~(STOP_BIT|SIGN_BIT); // drop sign as well
+	ret<<7;
+	ret |= in[1] & ~STOP_BIT;
+
+	// complement if negative
+	if(in[0] & SIGN_BIT) ret = ~ret;
+
+	*out = g_ntohs(ret);
+
+	return sizeof(gint16);
+}
+
+gint decode_uint16(
+	const guint8* in,
+	guint max_in_bytes,
+	guint16* out)
+{
+	if(count_encoded_bytes(in,max_in_bytes)!=sizeof(guint16))
+	{
+		return -1;
+	}
+
+	guint16 ret;
+
+	ret= in[0] & ~(STOP_BIT);
+	ret<<7;
+	ret |= in[1] & ~STOP_BIT;
+
+	*out = g_ntohs(ret);
+
+	return sizeof(guint26);
+}
+
+gint decode_int32(
+	const guint8* in,
+	guint max_in_bytes,
+	gint32* out)
+{
+	if(count_encoded_bytes(in,max_in_bytes)!=sizeof(gint32))
+	{
+		return -1;
+	}
+
+	gint32 ret;
+
+	ret= in[0] & ~(STOP_BIT|SIGN_BIT); // drop sign as well
+	ret<<7;
+	ret |= in[1] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[2] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[3] & ~STOP_BIT;
+
+	// complement if negative
+	if(in[0] & SIGN_BIT) ret = ~ret;
+
+	*out = g_ntohl(ret);
+
+	return sizeof(gint32);
+}
+
+gint decode_uint32(
+	const guint8* in,
+	guint max_in_bytes,
+	guint32* out)
+{
+	if(count_encoded_bytes(in,max_in_bytes)!=sizeof(guint32))
+	{
+		return -1;
+	}
+
+	guint32 ret;
+
+	ret= in[0] & ~(STOP_BIT);
+	ret<<7;
+	ret |= in[1] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[2] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[3] & ~STOP_BIT;
+
+	*out = g_ntohl(ret);
+
+	return sizeof(guint32);
+}
+
+gint decode_uint64(
+	const guint8* in,
+	guint max_in_bytes,
+	guint64* out)
+{
+	if(count_encoded_bytes(in,max_in_bytes)!=sizeof(guint64))
+	{
+		return -1;
+	}
+
+	guint64 ret;
+
+	ret = in[0] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[1] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[2] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[3] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[4] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[5] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[6] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[7] & ~STOP_BIT;
+
+	*out = g_ntoh64(ret);
+
+	return sizeof(guint64);
+}
+
+gint decode_int64(
+	const guint8* in,
+	guint max_in_bytes,
+	gint64* out)
+{
+	if(count_encoded_bytes(in,max_in_bytes)!=sizeof(gint64))
+	{
+		return -1;
+	}
+
+	gint64 ret;
+
+	ret = in[0] & ~(STOP_BIT|SIGN_BIT);
+	ret<<7;
+	ret |= in[1] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[2] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[3] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[4] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[5] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[6] & ~STOP_BIT;
+	ret<<7;
+	ret |= in[7] & ~STOP_BIT;
+
+	if(in[0]&SIGN_BIT) ret=~ret;
+
+	*out = g_ntoh64(ret);
+
+	return sizeof(gint64);
+}
+
+gint decode_ascii(
+	const guint8* in,
+	guint max_in_bytes,
+	gint8* out,
+	guint max_out_size)
+{
+	memset(out,0,max_out_size);
+
+	int i;
+	for(i=0;i<max_in_bytes && i<max_out_size-1;i++)
+	{
+		out[i]=in[i]& ~STOP_BIT;
+		if(in[i]&STOP_BIT)
+		{
+			return i+1;
+		}
+	}
+
+	return -1;
+}
+
+gint decode_bytes(
+	const guint8* in,
+	guint max_in_bytes,
+	guint8* out,
+	guint max_out_size)
+{
+	memset(out,0,max_out_size);
+
+	guint32 sz;
+	gint off=decode_uint32(in,max_in_bytes,&sz);
+	if(off==-1) return -1;
+
+	memcpy(out,in+off,sz>max_in_bytes?max_in_bytes:sz);
+
+	return sz;
+}
+
+gint decode_utf8(
+	const guint8* in,
+	guint max_in_bytes,
+	guint8* out,
+	guint max_out_size)
+{
+	memset(out,0,max_out_size);
+
+	// drop one byte for null terminator
+	return decode_bytes(in,max_in_bytes,out,max_out_size-1);
+}
+
+gint decode_flt10(
+	const guint8* in,
+	guint max_in_bytes,
+	gint32* out_wholepart,
+	gint32* out_decpart)
+{
+	return -1;
+}
+
+gint decode_fixdec(
+	const guint8* in,
+	guint max_in_bytes,
+	guint dec_bits,
+	gint32* out_wholepart,
+	gint32* out_decpart)
+{
+	return -1;
+}
---
+++ decode.h
@@ -0,0 +1,30 @@
+/**
+	@file	decode.h
+	@brief	|
+	@author	Wes Fournier
+
+	|
+*/
+
+#ifndef DECODE_H_INCLUDED_
+#define DECODE_H_INCLUDED_
+
+#include "fast.h"
+
+gint count_encoded_bytes(const guint8*,guint);
+
+gint decode_int16(const guint8*,guint,gint16*);
+gint decode_uint16(const guint8*,guint,guint16*);
+gint decode_int32(const guint8*,guint,gint32*);
+gint decode_uint32(const guint8*,guint,guint32*);
+gint decode_int64(const guint8*,guint,gint64*);
+gint decode_uint64(const guint8*,guint,gint64*);
+
+gint decode_ascii(const guint8*,guint,gint8*,guint);
+gint decode_bytes(const guint8*,guint,guint8*,guint);
+gint decode_utf8(const guint8*,guint,guint8*,guint);
+
+gint decode_flt10(const guint8*,guint,gint32*,gint32*);
+gint decode_fixdec(const guint8*,guint,guint,gint32*,gint32*);
+
+#endif
