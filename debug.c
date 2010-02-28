e7c41e0a80962d6d781865c022ac03589a74b511
jwf3188 <jwf3188@8d1846ee-05f3-11df-adb7-1be7f7e3f636>
2010-02-28 21:53:06 +0000

    debugging macros

---
+++ debug.c
@@ -0,0 +1,31 @@
+/**
+	@file	debug.c
+	@brief	|
+	@author	Wes Fournier
+
+	|
+*/
+
+#include "fast.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void debug_out(
+	const char* file,
+	const char* func,
+	int line,
+	const char* fmt,
+	...)
+{
+	char buf[1024];
+
+	sprintf(buf,"%s(%d) %s: %s\n",file,line,func,fmt);
+
+	va_list args;
+	va_start(args,fmt);
+
+	vprintf(buf,args);
+
+	va_end(args);
+}
