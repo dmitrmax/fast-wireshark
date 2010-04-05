dc11cb064c7a39cd45125aa7ab9a9ba1085c6f7c
SearonikMearshaktorn <SearonikMearshaktorn@8d1846ee-05f3-11df-adb7-1be7f7e3f636>
2010-04-05 20:10:06 +0000

    M    parse_template.c

--- parse_template.c
+++ parse_template.c
@@ -1,4 +1,5 @@
 #include "fast.h"
+#include "template.h"
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
 
@@ -11,7 +12,7 @@ void parse_xml(const char *);
 	parse_template is passed a node to a template and tries to read the things inside of it. 
 	****** currently only finds null. by output it seems to be an issue with parse_xml not parsing the file right
 *****/
-void parse_template(xmlDocPtr, xmlNodePtr);
+void parse_template(xmlDocPtr, xmlNodePtr, struct template_type**);
 
 /*****
 	Code for parse_xml. Comments below
@@ -57,11 +58,21 @@ void parse_xml(const char* template){
 	/*****
 	Begin main parins loop. get the first child of the root, then loop from there. Any "template" nodes imply we have a 		template to parse, so we send it on over to parse_template!
 	*****/
-	cur = cur->xmlChildrenNode;
+	if (cur->name != (const xmlChar *)"template")
+		cur = cur->xmlChildrenNode;
 	while (cur != NULL) {
 /*		printf("Node: %s.\n", cur->name);*/
 		if ((!xmlStrcmp(cur->name, (const xmlChar *)"template"))) {
-			parse_template(doc, cur->xmlChildrenNode);
+			xmlChar * name;
+			xmlChar * id;
+			struct template_type ** template;
+			name = xmlGetProp(cur,(const xmlChar*) "name");
+			id = xmlGetProp(cur,(const xmlChar*) "id");
+			printf("Name: %s. \nID: %s \n", name, id);
+			create_template((const char *)name, atoi((const char*)id), template);
+			parse_template(doc, cur->xmlChildrenNode, template);
+			xmlFree(name);
+			xmlFree(id);
 		}
 
 		cur = cur->next;
@@ -76,35 +87,39 @@ void parse_xml(const char* template){
 }
 /*****
 our lovely Parse-template function. This function will eventually build our templates we use, but for now it just tries to traverse them
-***** WARNING - Fuinction does not work. possibly an issue in parse_xml, possibly an issue in how we are trying to do things and
-	we are doing them wrong. *****
 *****/
-void parse_template(xmlDocPtr doc, xmlNodePtr cur) {
+void parse_template(xmlDocPtr doc, xmlNodePtr cur, struct template_type** template) {
 
 	/*****
 	convience vairable for storing values
 	*****/
-	xmlChar *key;
-/*	puts("Parsing...kinda.\n");*/
-	/*****
-	move along to the child, then parse
-	*****/
-	cur = cur->xmlChildrenNode;
+	xmlChar *name;
+/*	xmlChar *value;*/
+	xmlChar *id;
+	xmlChar *presence;
 	/*****
 	this means we have nothing to do, me thinks
 	*****/
 /*	if (cur == NULL){
 		printf("Well that was a bust, the node is NULL!\n");
 	}*/
+
+		/* Time to start on with sorting throguh the nodes. */ 
 		while (cur != NULL) {
 			/*****
 			Find the element of the current node and move into more looping
 			*****/		
-				key = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-			printf("keyword: %s\n, node name: %s", key, cur->name);
-			xmlFree(key);
+			name = xmlGetProp(cur,(const xmlChar*) "name");
+			id = xmlGetProp(cur,(const xmlChar*) "id");
+			presence = xmlGetProp(cur,(const xmlChar*) "presence");
+/*			printf("Propname: %s \nnode name: %s \nPropPresence: = %s \nPropid: %s \n", name, cur->name, presence, id);*/
+			if (cur->xmlChildrenNode != NULL){
+	/*			printf("SubNodeName: %s \n", cur->xmlChildrenNode->name);*/
+			}
+			xmlFree(name);
+			xmlFree(presence);
+			xmlFree(id);
 			cur = cur->next;
-	/*		puts("PARSE PARSE PARSE \n"); - Silly debugging statement */
 		}
 	/*****
 	all done here, nothing to see, move along now
