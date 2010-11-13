f6d68d6fc8cb5e67f64f523cab689e3537b1e83f
grencez <grencez@8d1846ee-05f3-11df-adb7-1be7f7e3f636>
2010-11-13 20:25:56 +0000

    Add Derek's changes to the Windows bat scripts.

--- install.bat
+++ install.bat
@@ -1,5 +1,9 @@
 
-rem This should do everything.
+IF EXIST "%HOMEPATH%\Application Data\Wireshark\plugins" GOTO COPYDLL
+mkdir "%HOMEPATH%\Application Data\Wireshark\plugins"
+:COPYDLL
+
 copy /B /Y fast.dll "%HOMEPATH%\Application Data\Wireshark\plugins\fast.dll"
+
 pause
 
--- test/wireshark.bat
+++ test/wireshark.bat
@@ -1,4 +1,9 @@
 
-"C:\Program Files\Wireshark\wireshark" -r "output.pcap" -o fast.port:"5000" -o fast.template:"templates.xml"
+"C:\Program Files\Wireshark\wireshark" -r "output.pcap" ^
+  -o fast.port:"5000" ^
+  -o fast.template:"templates.xml" ^
+  -o fast.enabled:true ^
+  -o fast.show_empty:true
+
 pause
 
