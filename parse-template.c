f9f061cd5c4829401924b6d48807f85734ad7647
messybricks <messybricks@8d1846ee-05f3-11df-adb7-1be7f7e3f636>
2011-04-18 21:39:45 +0000

    Issue 91: added header comment to every c and h file

--- parse-template.c
+++ parse-template.c
@@ -32,6 +32,23 @@ static gboolean check_valid_operator(xmlNodePtr xmlnode, FieldType * tfield);
 static gboolean is_integer(FieldTypeIdentifier type);
 
 static gint templateID = -1; /* Stores tid while parsing */
+/*
+* This file is part of FAST Wireshark.
+*
+* FAST Wireshark is free software: you can redistribute it and/or modify
+* it under the terms of the Lesser GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* FAST Wireshark is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* Lesser GNU General Public License for more details.
+*
+* You should have received a copy of the Lesser GNU General Public License
+* along with FAST Wireshark.  If not, see 
+* <http://www.gnu.org/licenses/lgpl.txt>.
+*/
 
 /*! \brief  Convert an XML file into an internal representation of
  *          the templates.
